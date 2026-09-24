0324976c75864eb7599833c9ba4026a90deb3f38
Muhammad Sufyan <sufyanmk2024801@gmail.com>
2026-09-24 15:51:24 +0500

    updated api.js file api issues

--- sockets/api.js
+++ sockets/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = window.location.hostname.endsWith(".devtunnels.ms")
   ? import.meta.env.VITE_TUNNEL_API_URL
-  : import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  : import.meta.env.VITE_API_URL || "https://chat-app-0aam.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
